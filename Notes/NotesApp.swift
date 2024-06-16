02adbfe1794cbc7ebd77d7e7c74cfc6a41d37b26
rmj0025 <127268170+rmj0025@users.noreply.github.com>
2024-06-16 16:32:59 -0500

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Marion Jury on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
